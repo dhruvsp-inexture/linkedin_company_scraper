db9117d04ec64545b48d42b02b7a31a0642b6ff3
DHRUV <dhruvsp.inexture@gmail.com>
2023-04-04 10:36:35 +0530

    selected location from the locations checkbox filter.

--- linkedin_scraper.py
+++ linkedin_scraper.py
@@ -1,7 +1,11 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from dotenv import load_dotenv
 import os
+
+from selenium.webdriver.support.wait import WebDriverWait
 
 load_dotenv()
 
@@ -20,3 +24,16 @@ login_form_password.send_keys(os.environ.get("PASSWORD"))
 login_submit_button = driver.find_element(By.CLASS_NAME, "from__button--floating").click()
 
 driver.get("https://www.linkedin.com/search/results/companies/")
+
+all_filters_button = WebDriverWait(driver, timeout=10).until(
+    lambda d: d.find_element(By.CSS_SELECTOR, "div.relative.mr2 button.artdeco-pill")).click()
+
+locations_checkboxes = WebDriverWait(driver, timeout=10).until(
+    lambda d: d.find_element(By.CLASS_NAME, "search-reusables__secondary-filters-values"))
+all_checkboxes = WebDriverWait(locations_checkboxes, timeout=10).until(
+    lambda d: d.find_elements(By.TAG_NAME, "li"))
+north_america_location_checkbox = (all_checkboxes[0]).find_element(By.TAG_NAME, "label").click()
+custom_location_checkbox = all_checkboxes[-1].find_element(By.TAG_NAME, "button").click()
+custom_location = all_checkboxes[-1].find_element(By.TAG_NAME, "input")
+custom_location.send_keys("India")
+time.sleep(5)
